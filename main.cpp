be98f042f66bf13269e94d3e512f1698e57bf5f5
nmorales9 <nmorales9@mail.csuchico.edu>
2018-02-15 16:34:26 -0800

    Description of what this version does

--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@ int main()
   string lastname;
   string nickname;
   int lucky;
+  char firstchar;
+  char lastchar;
 
   //get user input
 
@@ -19,11 +21,28 @@ int main()
   cin>> firstname;
   cout<< "What is your last name?\n";
   cin>> lastname;
-
   //tell fortune
- lucky= firstname.length();
- cout<< "welcome, "<<firstname[0]<<"."<<lastname[0]<<"., here is your fortune...\n.";
-cout<< "Your lucky numer is.. "<<lucky<<endl;
+   lucky= firstname.length();
+    cout<< "welcome, "<<firstname[0]<<"."<<lastname[0]<<"., here is your fortune...\n";
+    cout<< "Your lucky numer is.. "<<lucky<<endl;
+
+
+
+if (firstchar== 'a' ||firstchar== 'e' ||firstchar== 'i'||firstchar== 'o'||firstchar== 'u'||firstchar== 'A' ||firstchar== 'E' ||firstchar== 'I' ||firstchar== 'O' ||firstchar== 'U')
+  {
+  cout<< "Your destined to be famous!\n";
+  }
+  else 
+  {
+  cout << "you should keep a low profile.\n";
+  }
+ 
+ if (lastchar== 'a' ||lastchar== 'e' ||lastchar== 'i' ||lastchar== 'o' ||lastchar== 'u' ||lastchar== 'A' ||lastchar== 'E' ||lastchar== 'I' ||lastchar== 'O' ||lastchar== 'U')
+  {
+  cout<< "you already have met your true love.\n";
+  }
+
+ cout<< "Have a good day!\n";
 
   return 0;
 }
